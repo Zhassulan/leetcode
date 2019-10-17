352b143cc54e13c50a744ffa239156dda20300c1
JV\Zhassulan.Tokbayev <zhassulan.tokbayev@tele2.com>
2019-10-17 14:16:15 +0600

    tests

--- src/main/java/kz/tele2/study/LongestSubstringLen/Solution.java
+++ src/main/java/kz/tele2/study/LongestSubstringLen/Solution.java
@@ -15,30 +15,33 @@ import java.util.HashSet;
 public class Solution {
 
     public int lengthOfLongestSubstring(String s) {
+        if (s.length() == 0 || s == "" || s == null)    {
+            return 0;
+        }
         System.out.println("s = " + s);
-        int l_max = 1;
-        System.out.println("l_max = " + l_max);
         int n = s.length();
+        int l_max = n;
         System.out.println("n = " + n);
         int k = 0, i = 0, j = 0;
         String sub = null, searchStr = null;
-        for (k = 0; k < n / 2; k++) {
+        for (k = 0; k < n; k++) {
             System.out.println("----------------------------");
             System.out.println("k = " + k);
             for (i = 0; i < n - k; i++) {
                 System.out.println("i = " + i);
                 sub = s.substring(i, i + k + 1);
+                System.out.println("l_max = " + l_max);
                 System.out.println("sub = " + sub);
                 if (k > 0)  {
                     if (firstRepeating(sub))
                         continue;
                 }
                 searchStr = s.substring(i + k + 1, n);
+                System.out.println("searchStr = " + searchStr);
                 if (searchStr.length() < sub.length())  {
                     l_max = k + 1;
                     continue;
                 }
-                System.out.println("searchStr = " + searchStr);
                 if (searchStr.contains(sub)) {
                     l_max = k + 1;
                     System.out.println("found, l_max = " + l_max);
--- src/test/java/kz/tele2/study/LengthOfLongestSubstring.java
+++ src/test/java/kz/tele2/study/LengthOfLongestSubstring.java
@@ -10,11 +10,38 @@ public class LengthOfLongestSubstring {
 
     @Test
     public void testSolution()  {
-        //String s = "abcabcbb"; //3
-        String s = "bbbbb"; //1
-        //String s = "pwwkew"; //3
-        Solution solution = new Solution();
-        assertEquals(1, solution.lengthOfLongestSubstring(s));
+        assertEquals(3, new Solution().lengthOfLongestSubstring("abcabcbb"));
     }
+
+    @Test
+    public void testSolution1()  {
+        assertEquals(1, new Solution().lengthOfLongestSubstring("bbbbb"));
+    }
+
+    @Test
+    public void testSolution2()  {
+        assertEquals(3, new Solution().lengthOfLongestSubstring("pwwkew"));
+    }
+
+    @Test
+    public void testSolution3()  {
+        assertEquals(0, new Solution().lengthOfLongestSubstring(""));
+    }
+
+    @Test
+    public void testSolution4()  {
+        assertEquals(2, new Solution().lengthOfLongestSubstring("au"));
+    }
+
+    @Test
+    public void testSolution5()  {
+        assertEquals(3, new Solution().lengthOfLongestSubstring("abc"));
+    }
+
+    @Test
+    public void testSolution6()  {
+        assertEquals(5, new Solution().lengthOfLongestSubstring("hkcpmprxxxqw"));
+    }
+
 
 }
